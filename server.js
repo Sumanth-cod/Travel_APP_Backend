bc2b01bc2042083205b00d2b9c85d17f67d719c3
sumanth-cod <sumanthkomirisetty77319@gmail.com>
2025-06-10 21:02:41 +0530

    final commit

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ app.use('/api/hotels',singlehotelrouter)
 app.use('/api/auth',authrouter);
 app.use('/api/wishlist',Wishlistrouter)
 
-const PORT = process.env.port || 3000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server running on port ${PORT}`);
